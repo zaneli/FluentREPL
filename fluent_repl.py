04c4c03aa778e773f426b86b5b63e4d669ed5fd2
zaneli <shun.otani@gmail.com>
2012-11-25 00:02:05 +0900

    first commit

---
+++ fluent_repl.py
@@ -0,0 +1,41 @@
+import sublime, sublime_plugin, os, re
+
+class FluentReplTypeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        replView = get_ghci_view(self.view.window().views())
+        if replView is None:
+            return
+
+        sels = self.view.sel()
+        for sel in sels:
+            word = self.view.substr(self.view.word(sel))
+            if not re.match(r"^\w+$", word) and not re.match(r"^\(.*\)$", word):
+                word = "(" + word + ")"
+
+            put_ghci_command(replView, "t", word)
+
+class FluentReplAutoLoadEventListener(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        filename = view.file_name()
+        path, ext = os.path.splitext(filename)
+
+        if ext != ".hs":
+            return
+
+        replView = get_ghci_view(view.window().views())
+        if replView is None:
+            return
+
+        put_ghci_command(replView, "l", path)
+
+def get_ghci_view(views):
+    for view in views:
+        if view.name() == "*REPL* [haskell]":
+            return view
+    return None
+
+def put_ghci_command(view, command, param):
+    edit = view.begin_edit()
+    view.insert(edit, view.size(), ":" + command + " " + param)
+    view.run_command("repl_enter")
+    view.end_edit(edit)
